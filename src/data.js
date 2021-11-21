6109979e0b79f20601f2f88f7515b1271dc1342a
thanawat1112 <59079111+thanawat1112@users.noreply.github.com>
2021-11-22 04:27:19 +0700

    er

--- src/data.js
+++ src/data.js
@@ -37,5 +37,43 @@ const data = {
       image: 'https://cf.lnwfile.com/10j7zg.jpg',
     },
   ],
+  products: [
+    {
+      id: '1',
+      name: 'M200',
+      price: 12500,
+      image:'https://www.igetweb.com/uploads/7877/filemanager/6fb88e023308f70b6e4fc8995cdd600e_full.jpg',
+    },
+    {
+      id: '2',
+      name: 'M110',
+      price: 7900,
+      image: 'https://www.igetweb.com/uploads/7877/filemanager/fb31f46b2e65b17e5bcefe241e03d8bc_full.jpg',
+    },
+    {
+      id: '3',
+      name: 'L96',
+      price: 7600,
+      image: 'https://dc.lnwfile.com/43o2bt.jpg',
+    },
+    {
+      id: '4',
+      name: 'Tar21',
+      price: 6600,
+      image: 'https://cf.lnwfile.com/f0pras.jpg',
+    },
+    {
+      id: '5',
+      name: 'G36',
+      price: 4600,
+      image: 'https://cf.lnwfile.com/yl4oal.jpg',
+    },
+    {
+      id: '6',
+      name: 'P90',
+      price: 5600,
+      image: 'https://cf.lnwfile.com/10j7zg.jpg',
+    },
+  ],
 };
 export default data;
